f4c247e4dbad86156bebd4712ebdf427deb3550f
polunga <naopan.milktea.love@gmail.com>
2015-12-14 17:07:07 +0900

    Alamofire インストール

---
+++ SwitchProject/AddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddViewController.swift
+//  SwitchProject
+//
+//  Created by Naoya Kurahashi on 2015/12/14.
+//  Copyright © 2015年 Naoya Kurahashi. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
