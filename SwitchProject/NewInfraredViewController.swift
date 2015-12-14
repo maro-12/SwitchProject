f422289f13b51101d9d6c8ec2767e9e4eb8ba167
polunga <naopan.milktea.love@gmail.com>
2015-12-15 06:41:18 +0900

    新規赤外線登録ページ実装

--- SwitchProject/NewInfraredViewController.swift
+++ SwitchProject/NewInfraredViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MBCircularProgressBar
+import Alamofire
 
 class NewInfraredViewController: UIViewController , UITextFieldDelegate {
 
@@ -17,16 +18,18 @@ class NewInfraredViewController: UIViewController , UITextFieldDelegate {
 //    nameTextField.borderStyle = UITextBorderStyle.RoundedRect
 //    
     let localdata = NSUserDefaults.standardUserDefaults()
-    var timer : NSTimer!
+    var timer:NSTimer!
     var irName:String?
+    var groupID:Int?
     var ir_id:Int?
+    var newIrName:String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.13, target: self, selector: "update", userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.12, target: self, selector: "update", userInfo: nil, repeats: true)
         
-        postInfraredData()
+        postInfrared()
         // Do any additional setup after loading the view.
     }
 
@@ -38,79 +41,131 @@ class NewInfraredViewController: UIViewController , UITextFieldDelegate {
     @IBOutlet weak var timeBar: MBCircularProgressBarView!
     
     func update(){
-        if(timeBar.value < 100){
-            timeBar.value++
+        if(timeBar.value > 0.08){
+            timeBar.value += -0.08
         }else{
-            timer.invalidate()
             timeBar.value = 0
-            timeBar.emptyLineColor = UIColor.redColor()
+            timer.invalidate()
+            timeBar.emptyLineColor = UIColor.mcOrange200()
             messageLabel.text = "取得できませんでした"
         }
     }
     
     func inputAlert() {
-        var inputTextField: UITextField?
-//        var passwordField: UITextField?
-        
+        var infraredNameTextField: UITextField?
+        var new_Ir_name:String?
+//        var groupNameTextField: UITextField?
+
         let alertController: UIAlertController = UIAlertController(title: "名前をつけてね", message: "Input infrared name", preferredStyle: .Alert)
         
+        alertController.addTextFieldWithConfigurationHandler { textField -> Void in
+            infraredNameTextField = textField
+            textField.placeholder = "赤外線の名前"
+        }
+//        alertController.addTextFieldWithConfigurationHandler{textField -> Void in
+//            groupNamrTextField = textField
+//            textField.placeholder = "グループID(任意)"
+//        }       
+
         let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
-            print("Cancel")
         }
+        let logintAction: UIAlertAction = UIAlertAction(title: "Done", style: .Default) { action ->  Void in
+            new_Ir_name = infraredNameTextField!.text
+            self.putInfraredRename(new_Ir_name!)
+        }
+
         alertController.addAction(cancelAction)
-        
-        let logintAction: UIAlertAction = UIAlertAction(title: "Done", style: .Default) { action -> Void in
-//            print("Done")
-//            print(inputTextField?.text)
-//            print(passwordField?.text)
-            self.putInfraredData(self.irName!)
-        }
         alertController.addAction(logintAction)
         
-        alertController.addTextFieldWithConfigurationHandler { textField -> Void in
-            inputTextField = textField
-            textField.placeholder = "赤外線の名前"
-            self.irName = inputTextField!.text
-        }
-        alertController.addTextFieldWithConfigurationHandler{textField -> Void in
-            inputTextField = textField
-            textField.placeholder = "グループID(任意)"
-        }
-//        alertController.addTextFieldWithConfigurationHandler { textField -> Void in
-//            passwordField = textField
-//            textField.secureTextEntry = true
-//            textField.placeholder = "password"
-//        }
-        
+ 
         presentViewController(alertController, animated: true, completion: nil)
     }
+
+    func successAlert(){
+        let successAlertController: UIAlertController = UIAlertController(title: "Success!!", message: "New infrared name is \(self.newIrName!)", preferredStyle: .Alert)
     
-    func postInfraredData(){
-        let urlHead:String = self.localdata.objectForKey("siteURL") as! String
-        // apiで取得するためのURLを指定
-        let auth_token = String(self.localdata.objectForKey("auth_token")!)
-        let URL = NSURL(string: "\(urlHead):80/api/v1/ir/receive.json")
-        let req = NSMutableURLRequest(URL:URL!)
+        let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .Default) { action -> Void in
+        }
+        successAlertController.addAction(okAction)
+
+        presentViewController(successAlertController, animated: true, completion: nil)
+    }
+
+    func failedAlert(){
+        let failedAlertController: UIAlertController = UIAlertController(title: "Error", message: "Infrared rename is failed..", preferredStyle: .Alert)
+    
+        let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .Default) { action -> Void in
+        }
+        failedAlertController.addAction(okAction)
+
+        presentViewController(failedAlertController, animated: true, completion: nil)
+    }
+    
+    // func postInfraredData(){
+    //     let urlHead:String = self.localdata.objectForKey("siteURL") as! String
+    //     // apiで取得するためのURLを指定
+    //     let auth_token = String(self.localdata.objectForKey("auth_token")!)
+    //     let URL = NSURL(string: "\(urlHead):80/api/v1/ir/receive.json")
+    //     let req = NSMutableURLRequest(URL:URL!)
         
         
-        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: configuration, delegate:nil, delegateQueue:NSOperationQueue.mainQueue())
+    //     let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+    //     let session = NSURLSession(configuration: configuration, delegate:nil, delegateQueue:NSOperationQueue.mainQueue())
         
         
-        req.HTTPMethod = "POST"
-        req.HTTPBody = "auth_token=\(auth_token)".dataUsingEncoding(NSUTF8StringEncoding)
+    //     req.HTTPMethod = "POST"
+    //     req.HTTPBody = "auth_token=\(auth_token)".dataUsingEncoding(NSUTF8StringEncoding)
         
-        let task = session.dataTaskWithRequest(req, completionHandler: {
-            (data, response, error) -> Void in
-            do{
-                let json = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments )
+    //     let task = session.dataTaskWithRequest(req, completionHandler: {
+    //         (data, response, error) -> Void in
+    //         do{
+    //             let json = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments )
                 
-                let res:NSDictionary = json.objectForKey("meta") as! NSDictionary
-                let resResponse:NSDictionary = json.objectForKey("response") as! NSDictionary
-                let infrared:NSDictionary = resResponse.objectForKey("infrared") as! NSDictionary
-                if String(res["status"]!) == "201"{
+    //             let res:NSDictionary = json.objectForKey("meta") as! NSDictionary
+    //             let resResponse:NSDictionary = json.objectForKey("response") as! NSDictionary
+    //             let infrared:NSDictionary = resResponse.objectForKey("infrared") as! NSDictionary
+    //             if String(res["status"]!) == "201"{
+    //                 self.messageLabel.text = "成功！名前をつけてください！"
+    //                 self.ir_id = infrared["id"] as? Int
+    //                 self.inputAlert()
+    //             }else{
+    //                 print("取得できませんでした")
+    //             }
+    //         }catch{
+    //             print("Error")
+    //         }
+            
+    //     })
+    //     print(ir_id)
+    //     task.resume()
+    // }
+
+    func postInfrared(){
+        let urlHead:String = self.localdata.objectForKey("siteURL") as! String
+        let auth_token = String(self.localdata.objectForKey("auth_token")!)
+        var getStatus:Int?
+        var interimID:Int?
+
+        Alamofire.request(.POST , "\(urlHead):80/api/v1/ir/receive.json" ,
+                        parameters:["auth_token":"\(auth_token)"]).response{(request , response , data , error) in 
+            do{
+                var obj : AnyObject? = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                if let meta = obj!["meta"] as? [String : AnyObject]{
+                    if let status = meta["status"] as? Int{
+                        getStatus = status
+                    }
+                }
+                if let response = obj!["response"] as? [String:AnyObject]{
+                    if let infrared = response["infrared"] as? [String:AnyObject]{
+                        if let id = infrared["id"] as? Int{
+                            interimID = id
+                        }
+                    }
+                }
+                if(getStatus! == 201){
                     self.messageLabel.text = "成功！名前をつけてください！"
-                    self.ir_id = infrared["id"] as? Int
+                    self.ir_id = interimID
+                    self.timer.invalidate()
                     self.inputAlert()
                 }else{
                     print("取得できませんでした")
@@ -118,52 +173,83 @@ class NewInfraredViewController: UIViewController , UITextFieldDelegate {
             }catch{
                 print("Error")
             }
+        }
+    }   
+
+//     func putInfraredData(irName:String){
+//         let urlHead:String = self.localdata.objectForKey("siteURL") as! String
+//         // apiで取得するためのURLを指定
+//         let auth_token = String(self.localdata.objectForKey("auth_token")!)
+//         let URL = NSURL(string: "\(urlHead):80/api/v1/ir/rename.json")
+//         let req = NSMutableURLRequest(URL:URL!)
+//         let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+//         let session = NSURLSession(configuration: configuration, delegate:nil, delegateQueue:NSOperationQueue.mainQueue())
+        
+// //        let name:String = self.irName
+        
+//         req.HTTPMethod = "PUT"
+//         req.HTTPBody = "auth_token=\(auth_token)&name=\(irName)&ir_id\(ir_id)".dataUsingEncoding(NSUTF8StringEncoding)
+// //        req.setValue(auth_token, forHTTPHeaderField: "auth_token")
+// //        req.setValue(irName, forHTTPHeaderField: "name")
+// //        req.setValue(ir_id, forHTTPHeaderField:"ir_id")
+
+//         let task = session.dataTaskWithRequest(req, completionHandler: {
+//             (data, response, error) -> Void in
+//             do{
+//                 let json = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments )
+//                 let res:NSDictionary = json.objectForKey("meta") as! NSDictionary
+// //                let resResponse:NSDictionary = json.objectForKey("response") as! NSDictionary
+// //                let infrared:NSDictionary = resResponse.objectForKey("infrared") as! NSDictionary
+
+//                 if String(res["status"]!) == "201"{
+// //                    let name:String = infrared["name"] as! String
+//                     self.messageLabel.text = "成功！名前は\(irName)です"
+//                     // self.inputAlert()
+//                 }else{
+//                     print("取得できませんでした")
+//                 }
+//             }catch{
+//                 print("Error")
+//             }
             
-        })
-        print(ir_id)
-        task.resume()
-    }
+//         })
+        
+//         task.resume()
+//     }
 
-    func putInfraredData(irName:String){
+    func putInfraredRename(infrared_name:String?){
         let urlHead:String = self.localdata.objectForKey("siteURL") as! String
-        // apiで取得するためのURLを指定
         let auth_token = String(self.localdata.objectForKey("auth_token")!)
-        let URL = NSURL(string: "\(urlHead):80/api/v1/ir/rename.json")
-        let req = NSMutableURLRequest(URL:URL!)
-        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: configuration, delegate:nil, delegateQueue:NSOperationQueue.mainQueue())
-        
-//        let name:String = self.irName
-        
-        req.HTTPMethod = "PUT"
-        req.HTTPBody = "auth_token=\(auth_token)&name=\(irName)&ir_id\(ir_id)".dataUsingEncoding(NSUTF8StringEncoding)
-//        req.setValue(auth_token, forHTTPHeaderField: "auth_token")
-//        req.setValue(irName, forHTTPHeaderField: "name")
-//        req.setValue(ir_id, forHTTPHeaderField:"ir_id")
-
-        let task = session.dataTaskWithRequest(req, completionHandler: {
-            (data, response, error) -> Void in
+        var getStatus:Int?
+        self.newIrName = infrared_name
+        Alamofire.request(.PUT , "\(urlHead):80/api/v1/ir/rename.json" , 
+                        parameters:["auth_token":"\(auth_token)" ,
+                                    "name"      :"\(infrared_name!)",
+                                    "ir_id"     :"\(self.ir_id!)"
+                                    ]).response{(request , response , data , error ) in
             do{
-                let json = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.AllowFragments )
-                let res:NSDictionary = json.objectForKey("meta") as! NSDictionary
-//                let resResponse:NSDictionary = json.objectForKey("response") as! NSDictionary
-//                let infrared:NSDictionary = resResponse.objectForKey("infrared") as! NSDictionary
-
-                if String(res["status"]!) == "201"{
-//                    let name:String = infrared["name"] as! String
-                    self.messageLabel.text = "成功！名前は\(irName)です"
-                    // self.inputAlert()
+                var obj : AnyObject? = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                if let meta = obj!["meta"] as? [String:AnyObject]{
+                    print("fuga")
+                    if let status = meta["status"] as? Int{
+                        getStatus = status
+                    }
+//                    if let message = meta["message"] as? String{
+//                        // print(message)
+//                    }
+                }
+                if(getStatus! == 200){
+                    print("りねいむ完了")
+                    self.successAlert()
                 }else{
-                    print("取得できませんでした")
+                    print("りねいむ失敗")
+                    self.failedAlert()
                 }
             }catch{
-                print("Error")
+                print("Error putInfraredRename")
             }
-            
-        })
-        
-        task.resume()
-    }
+        }
+    }    
 
 
     /*
